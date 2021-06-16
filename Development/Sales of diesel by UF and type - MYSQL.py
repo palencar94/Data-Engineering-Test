1dd94b3f75e9e9ce3b91c648e0e94acc72628583
Paulo Guilherme Alencar <57593367+palencar94@users.noreply.github.com>
2021-06-16 12:25:33 -0300

    Update Sales of diesel by UF and type - MYSQL.py

--- Development/Sales of diesel by UF and type - MYSQL.py
+++ Development/Sales of diesel by UF and type - MYSQL.py
@@ -1,43 +1,42 @@
 
 # coding: utf-8
 
-# In[3]:
+# Importar as bibliotecas
 
 
 import pymysql.cursors
 import pandas as pd
 import mysql.connector
 from mysql.connector import MySQLConnection
-
-
-# In[4]:
-
-
 from sqlalchemy import create_engine
+
+
+# Conexão com o container mysql
+
 
 engine = create_engine('mysql+pymysql://root:secret@172.18.0.5/oil?charset=utf8')
 
 
-# In[6]:
+# Leitura do csv
 
 
 final = pd.read_csv('final_diesel.csv')
 
 
-# In[7]:
+# Validação
 
 
 #final.set_index('YEAR_MONTH', inplace=True)
 final
 
 
-# In[8]:
+# Export do dataframe para o mysql em uma tabel de stage
 
 
 final.to_sql('stg_diesel', con = engine, if_exists = 'replace', index = False)
 
 
-# In[9]:
+# Conexao com o container mysql
 
 
 conn = mysql.connector.connect(host='172.18.0.5',
@@ -48,7 +47,7 @@ cursor_create = conn.cursor()
 cursor = conn.cursor()
 
 
-# In[18]:
+# carga da tabela de stage para a tabela de produção e query com o indicador solicitado no desafio
 
 
 cursor_create.execute("CREATE TABLE ods_diesel PARTITION BY HASH(MONTH(`YEAR_MONTH`)) AS SELECT STR_TO_DATE(`YEAR_MONTH`, '%Y-%m-%d') AS `year_month`, UF AS ´uf´, `COMBUSTÍVEL` AS `product`, `UNIDADE` AS `unit`, `TOTAL` AS `volume`, CURRENT_TIMESTAMP() AS `created_at` FROM stg_diesel")
@@ -56,13 +55,13 @@ cursor_create.execute("CREATE TABLE ods_diesel PARTITION BY HASH(MONTH(`YEAR_MON
 cursor.execute("SELECT ´uf´, `product`, SUM(`volume`) FROM ods_diesel GROUP BY 1, 2 ORDER BY 3 DESC")    
 
 
-# In[19]:
+# fecha o cursor das querys
 
 
 row = cursor.fetchone()
 
 
-# In[20]:
+# Mostra o resultado da query
 
 
 while row is not None:
